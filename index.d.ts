5cd5ca5ecec275ff6be7a39d01efa8af623f90b6
livelybone <liuzhiping@auraxy.com>
2019-11-03 17:27:59 +0800

    feat(index.d.ts): Update

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 import { Dispatch, SetStateAction } from 'react'
 
 interface MapPropToState<T extends any, P extends any> {
-  (props: P, preState?: T): T
+  (props: P, preState?: T, preProps?: P): T
 }
+
 declare function useStateTrackProp<P extends any>(
   props: P,
 ): [P, Dispatch<SetStateAction<P>>]
